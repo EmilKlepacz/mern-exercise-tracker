5be78d757626dd43b500b759a6769ef9d079acd0
Emil Klepacz <klepaczemil@gmail.com>
2020-05-06 20:17:59 +0200

    adding exercise to log implemented.

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css"
+import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 export default class CreateExercise extends Component {
 
@@ -23,11 +24,16 @@ export default class CreateExercise extends Component {
     }
 
     //right before anything is displayed on the page then load list of users
-    componentWillMount(){
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        });
+    componentDidMount(){
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if(response.data.length > 0){
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0]
+                    })
+                }
+            })
     }
 
     onChangeUsername(e){
@@ -66,7 +72,10 @@ export default class CreateExercise extends Component {
 
         console.log(exercise);
 
-        window.location = "/";
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
+
+        window.location = '/';
     }
 
     render() {
